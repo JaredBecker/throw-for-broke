f0ba3b02b949b05c36f99baf1bbd3ff9e61c962d
Jared-Becker <jaredbecker46@gmail.com>
2026-01-03 08:07:49 +0200

    Fix git issue

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,6 @@ import HelloWorld from "./components/HelloWorld.vue";
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
-    something changed
   </div>
   <HelloWorld msg="Vite + Vue" />
 </template>
